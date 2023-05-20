6c65e89b7af2625215ac31981037e337356ab244
Dawid Komisarczuk <komidawi@gmail.com>
2023-05-20 15:09:19 +0200

    Test: Add test for multiple products scan

--- src/test/java/com/github/komidawi/interview/domain/MainProcessorTest.java
+++ src/test/java/com/github/komidawi/interview/domain/MainProcessorTest.java
@@ -10,21 +10,41 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainProcessorTest {
-
-    private static final Money EXAMPLE_PRICE = new Money(10);
-    private final ProductProviderApi productProviderApi = barcode -> new Product(barcode, EXAMPLE_PRICE);
+    private final ProductProviderApi productProviderApi = barcode ->
+            new Product(barcode, new Money(Integer.parseInt(barcode.value())));
     private final ApiFacade mainProcessor = new MainProcessor(productProviderApi);
 
     @Test
     public void afterScanningItem_itsPriceIsAddedToTotalSum() {
         // given
-        Barcode barcode = new Barcode("123");
+        int itemPrice = 123;
+        Barcode barcode = new Barcode(String.valueOf(itemPrice));
 
         // when
         mainProcessor.scanItem(barcode);
 
         // then
         Money totalPrice = mainProcessor.getTotalPrice();
-        assertEquals(new Money(10), totalPrice);
+        assertEquals(new Money(itemPrice), totalPrice);
+    }
+
+    @Test
+    public void afterScanningTwoItems_theirTotalSumIsReturned() {
+        // given
+        int firstItemPrice = 123;
+        Barcode firstItemBarcode = new Barcode(String.valueOf(firstItemPrice));
+
+        // and
+        int secondItemPrice = 456;
+        Barcode secondItemBarcode = new Barcode(String.valueOf(secondItemPrice));
+
+        // when
+        mainProcessor.scanItem(firstItemBarcode);
+        mainProcessor.scanItem(secondItemBarcode);
+
+        // then
+        Money totalPrice = mainProcessor.getTotalPrice();
+        assertEquals(new Money(firstItemPrice + secondItemPrice), totalPrice);
     }
 }
+
